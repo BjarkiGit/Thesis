0b8da45682f030c6ce92ad95ca506c4398867b83
BjarkiGit <bjb66@hi.is>
2023-03-13 13:02:50 +0100

    Adding some files

---
+++ generate_maps.py
@@ -0,0 +1,152 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from glob import glob
+from natsort import natsorted
+# from Fitter_working import fit
+from line_fit import fit
+from mpdaf.obj import Cube
+from astropy.io import fits as f
+from models import gaussFit
+from loguru import logger
+
+
+# NAME = "J0156"
+# NAME = "J0004"
+# NAME = "J0139"
+# NAME = "J0232"
+NAME = "J2318"
+
+logger.add("logs/"+NAME+"_{time}.log")
+
+if NAME  == "J0004":
+# For J0004 # Run with SNR mask 1.5
+    PATH = "/home/bjarki/Documents/Thesis/Thesis-1/Data/J0004/"
+    DATA = PATH+"J0004_DATACUBE_FINAL.fits"
+    xRange = np.arange(140, 180, step=1) # J0004
+    yRange = np.arange(140, 180, step=1) # J0004
+    Z_INIT = 0.2386 # J0004
+    MASK = 1.5
+
+elif NAME == "J0139":
+# For J0139 # Run with SNR mask 1.5
+    PATH = "/home/bjarki/Documents/Thesis/Thesis-1/Data/J0139/"
+    DATA = PATH+"J0139_DATACUBE_FINAL.fits"
+    xRange = np.arange(140, 180, step=1) # J0139
+    yRange = np.arange(140, 180, step=1) # J0139
+    Z_INIT = 0.3073 # J0139
+    MASK = 1.5
+
+elif NAME == "J0156":
+    # For J0156 # Run with SNR mask 1
+    PATH = "/home/bjarki/Documents/Thesis/Thesis-1/Data/J0156/"
+    DATA = PATH+"J0156_DATACUBE_FINAL.fits"
+    xRange = np.arange(140, 180, step=1) # J0156
+    yRange = np.arange(130, 170, step=1) # J0156
+    Z_INIT = 0.2696 # J0156
+    MASK = 1
+
+elif NAME == "J0232":
+    # For J0232 # Run with SNR mask 1
+    PATH = "/home/bjarki/Documents/Thesis/Thesis-1/Data/J0232/"
+    DATA = PATH+"J0232_DATACUBE_FINAL.fits"
+    xRange = np.arange(140, 180, step=1) # J0232
+    yRange = np.arange(140, 180, step=1) # J0232
+    Z_INIT = 0.3095 # J0232
+    MASK = 1
+
+elif NAME == "J2318":
+    # For J2318 # Run with SNR mask 1
+    PATH = "/home/bjarki/Documents/Thesis/Thesis-1/Data/J2318/"
+    DATA = PATH+"J2318_DATACUBE_FINAL.fits"
+    xRange = np.arange(130, 200, step=1) # J2318
+    yRange = np.arange(130, 200, step=1) # J2318
+    Z_INIT = 0.2517 # J2318
+    MASK = 1
+
+else:
+    print("No data for galaxy "+NAME)
+    exit()
+
+lines = pd.read_csv("Lines.txt", sep = r"\s+")
+cube = Cube(filename = DATA)
+var = f.open(DATA)[2].data
+# The guess for z is what I think it "should" be, the FWHM guess is just random
+Z_VAL = Z_INIT
+WIDVAL = 2.0
+
+files = natsorted(glob(PATH+"results/"+"*.csv"))
+# There's probably a better way to set up this loop rather than the nested loops I have now
+for x in xRange:
+    # I feel like I want to pass in a different z when I go back to the other edge
+    # for now it's just the one I had at the start
+    for y in yRange:
+        name = PATH+"results/fitResult"+str(x)+"_"+str(y)+".csv"
+        printname = str(x)+","+str(y)
+        if name in files:
+            print("Fit of",printname,"exists, passing")
+            
+        else:
+            print("Running pixel ["+printname+"]")
+            result, df, fitResult, SNR = fit(cube, lines, var, x, y, Z_VAL, WIDVAL, MASK)
+            if result is not None:
+                z = result.params["z"]
+
+
+                print("Fit successfully")
+                # The dataframe doesn't like saving things of different lenghts
+                # so I set up a column with one value repeated all the way down
+                # to save in the dataframe for z, z_err, FWHM, and FWHM_err
+
+                # The try/except was set up to try to prevent it from breaking when z error
+                # can't be estimated
+                # This never returns Z_VAL as an array. I've checked that
+                length = np.ones_like(fitResult["flux"])
+                
+                try:
+                    zs = length*z.value
+                    zerrs = length*z.stderr
+ 
+                except Exception as e:
+                    zerrs=length*999
+                    logger.info("Got bad error estimation for pixel: ",
+                                 str(x)+","+str(y), "with SNR: ", str(round(SNR,5)))
+
+
+                wid = result.params["wid"]
+                WIDVAL = np.abs(wid.value)
+                ws = length*WIDVAL
+
+                try:
+                    werrs = length*wid.stderr
+
+                except Exception as e:
+                    werrs = length*999
+
+            else:
+                logger.info("Bad SNR ("+str(round(SNR,5))+") for pixel "+str(x)+","+str(y))
+                length = np.ones_like(fitResult["flux"])
+                zerrs = length*999
+                werrs = length*999
+                zs = length*0
+                ws = length*0
+
+            # Adding to the fitResult dataframe and saving with the name of the pixel
+            fitResult["z"] = zs
+            fitResult["zerr"] = zerrs
+            fitResult["FWHM"] = ws
+            fitResult["FWHMerr"] = werrs
+            fitResult.to_csv(name, index=False)
+            # print(name, "saved")
+
+            # I want to save a plot of all the fits, just haven't figured out how yet
+            """
+            WL = df["wl"].values
+
+            plt.plot(WL, df["flux0"], label="Flux")
+            plt.plot(WL, gaussFit(result.params, WL, lines = lines), label="Fit")
+            TITLE = "Pix ("+str(x)+","+str(y)+"), z="+str(Z_VAL)
+            plt.title(TITLE)
+            plt.legend(loc=(1,0))
+            plt.savefig(PATH+"results/figs/test/"+str(x)+"_"+str(y)+".pdf")
+            """
---
+++ plot_maps.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from astropy import constants as const
+
+
+"""
+This script loads the arrays saved by MakingArrays.py and plots them using
+custom colour maps. These are then saved as .pdf and .png files
+You just give it the path of the data and the name of the galaxy
+"""
+NAME = "J0156"
+# NAME = "J0004"
+# NAME = "J0139"
+# NAME = "J0232"
+# NAME = "J2318"
+PATH = "/home/bjarki/Documents/Thesis/Thesis-1/Data/"+NAME+"/results/maps/"
+c = const.c.value/1e3 # speed of light in km/s, used for kinematic plotting
+
+
+
+# Loading all the arrays
+Ha_mask = np.load(PATH+"Ha_mask.npy")
+Othree_mask = np.load(PATH+"Othree_mask.npy")
+OIII_4363 = np.load(PATH+"OIII_4363_arr.npy")
+z_mask = np.load(PATH+"z_mask.npy")
+z_mask_v = np.load(PATH+"z_mask_v.npy")
+O32 = np.load(PATH+"O32.npy")
+
+
+# Making the heatmaps with black nan values
+heat_map = cmap = plt.cm.get_cmap("hot").copy()
+cmap.set_bad('black',1.)
+gre = cmap = plt.cm.get_cmap("plasma").copy()
+cmap.set_bad('black',1.)
+
+# Plotting fluxes
+plt.imshow(Ha_mask, cmap=heat_map, interpolation="nearest",
+           norm=LogNorm(vmin=np.nanmin(Ha_mask), vmax=np.nanmax(Ha_mask)))
+cbar = plt.colorbar()
+cbar.set_label(r"10$^{-20}$ erg cm$^{-2} s^{-1}$")
+plt.suptitle(r"H$\alpha$", x=0.45)
+plt.title(NAME, fontsize = "small")
+plt.savefig(PATH+"figs/Ha.pdf")
+plt.savefig(PATH+"figs/Ha.png")
+plt.clf()
+
+plt.imshow(Othree_mask, cmap=heat_map, interpolation="nearest",
+           norm=LogNorm(vmin=np.nanmin(Ha_mask), vmax=np.nanmax(Ha_mask)))
+cbar2 = plt.colorbar()
+cbar2.set_label(r"10$^{-20}$ erg cm$^{-2} s^{-1}$")
+plt.suptitle(r"[OIII]", x=0.45)
+plt.title(NAME, fontsize = "small")
+plt.savefig(PATH+"figs/OIII.pdf")
+plt.savefig(PATH+"figs/OIII.png")
+plt.clf()
+
+try:
+    plt.imshow(OIII_4363, cmap=heat_map, interpolation="nearest",
+            norm=LogNorm(vmin=np.nanmin(OIII_4363), vmax=np.nanmax(OIII_4363)))
+    cbar3 = plt.colorbar()
+    cbar3.set_label(r"10$^{-20}$ erg cm$^{-2} s^{-1}$")
+    plt.suptitle(r"[OIII]$\lambda$4363", x=0.45)
+    plt.title(NAME, fontsize = "small")
+    plt.savefig(PATH+"figs/OIII_4363.pdf")
+    plt.savefig(PATH+"figs/OIII_4363.png")
+
+except Exception as e:
+    print("4363 bad",e)
+
+plt.clf()
+# Plotting OIII/OII ratio, colours here are up for reconsideration
+plt.imshow(O32, cmap=gre, interpolation="nearest", norm=LogNorm(vmin=0.1, vmax=5))
+cbar4 = plt.colorbar()
+plt.suptitle(r"[OIII]/[OII]", x=0.45)
+plt.title(NAME, fontsize = "small")
+plt.savefig(PATH+"figs/O32.pdf")
+plt.savefig(PATH+"figs/O32.png")
+plt.clf()
+
+# Making the colormap for kinematic maps with black nans
+cmap = plt.cm.get_cmap("coolwarm").copy()
+cmap.set_bad('black',1.)
+
+lim = 0.00015*c # The limits for the kinematic map
+plt.imshow(z_mask_v, cmap=cmap, interpolation="nearest", vmin = -lim, vmax = lim)
+cbar5 = plt.colorbar()
+cbar5.set_label(r"km s$^{-1}$")
+plt.suptitle("Velocity [km/s]", x=0.45)
+plt.title(NAME, fontsize = "small")
+plt.savefig(PATH+"figs/z.pdf")
+plt.savefig(PATH+"figs/z.png")
+plt.clf()
+
+# Logplot of redshift, unnormalised and not kinematic. Only experimental for now to
+# see more kinematics of the arm
+plt.imshow(z_mask, cmap="plasma", interpolation="nearest",
+           norm=LogNorm(vmin = 0.27-0.003, vmax = 0.27+0.0003))
+cbar5 = plt.colorbar()
+# cbar5.set_label(r"km s$^{-1}$")
+plt.suptitle(r"$z$", x=0.45)
+plt.title(NAME, fontsize="small")
+plt.savefig(PATH+"figs/z_log.pdf")
+plt.savefig(PATH+"figs/z_log.png")
+plt.clf()
